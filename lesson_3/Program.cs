dd10eea0031e5ee2c5ba451fb2a41dfbdf647091
Raxmatullo2002 <137624815+Raxmatullo2002@users.noreply.github.com>
2023-07-08 12:18:46 +0500

    Добавьте файлы проекта.

---
+++ lesson_3/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace lesson_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+           
+            int randomNumber = random.Next(1, 100);          
+            int number;          
+            int n = 0;
+
+            do
+            {
+                   Console.Write("Son kiriting (1 dan 100 gacha): ");
+                    
+                     number = Convert.ToInt32(Console.ReadLine());
+                   
+                     n++;
+
+                if (number > randomNumber)
+                {
+                    Console.WriteLine("Men oylagan son kichikroq");
+                }
+                else if (number < randomNumber)
+                {
+                    Console.WriteLine("Men oylagan son kattaroq");
+                }
+                else
+                {
+                    Console.WriteLine("TABRIKLAYMAN SIZ " +n+ " - urinishda topdingiz!");
+                    break;
+                }
+
+             } while (number != randomNumber) ;
+
+                     Console.ReadLine();
+        }
+    }
+}
